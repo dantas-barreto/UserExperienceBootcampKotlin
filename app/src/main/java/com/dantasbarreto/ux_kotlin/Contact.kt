add601f74597869c05752041b2c4f2a683ebd5ef
João Pedro Souza Dantas Barreto <jpsdb10@hotmail.com>
2021-03-26 17:41:05 -0300

    first commit

---
+++ app/src/main/java/com/dantasbarreto/ux_kotlin/Contact.kt
@@ -0,0 +1,7 @@
+package com.dantasbarreto.ux_kotlin
+
+data class Contact(
+        var name: String,
+        var phone: String,
+        var photo: String
+)
---
+++ app/src/main/java/com/dantasbarreto/ux_kotlin/MainActivity.kt
@@ -0,0 +1,88 @@
+package com.dantasbarreto.ux_kotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+
+    private val rvList: RecyclerView by lazy {
+        findViewById<RecyclerView>(R.id.rv_list)
+    }
+
+    private val adapter = ContactAdapter()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.drawer_menu)
+
+        initDrawer()
+        bindViews()
+        updateList()
+    }
+
+    private fun initDrawer() {
+        val drawerLayout = findViewById<View>(R.id.drawer_layout) as DrawerLayout
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_drawer, R.string.close_drawer)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+    }
+
+    private fun bindViews() {
+        rvList.adapter = adapter
+        rvList.layoutManager = LinearLayoutManager(this)
+    }
+
+    private fun updateList() {
+        adapter.updateList(
+            arrayListOf(
+                Contact(
+                    "João Pedro",
+                    "(19) 99740-2386",
+                    "img.png"
+                ),
+                Contact(
+                    "João Pedro",
+                    "(19) 99740-2386",
+                    "img.png"
+                )
+            )
+        )
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.item_menu_1 -> {
+                showToast("Exibindo item de menu 1")
+                true
+            }
+            R.id.item_menu_2 -> {
+                showToast("Exibindo item de menu 2")
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
